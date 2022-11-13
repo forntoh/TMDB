b658cfb77716680c0460cd3fe09ae086ad398ee2
Forntoh Thomas <thomasforntoh@gmail.com>
2022-11-13 22:17:41 +0100

    added entities

---
+++ common/src/main/java/dev/forntoh/common/entities/Genre.kt
@@ -0,0 +1,10 @@
+package dev.forntoh.common.entities
+
+
+import androidx.annotation.Keep
+
+@Keep
+data class Genre(
+    val id: Int,
+    val name: String
+)
---
+++ common/src/main/java/dev/forntoh/common/entities/Movie.kt
@@ -0,0 +1,19 @@
+package dev.forntoh.common.entities
+
+import androidx.annotation.Keep
+
+@Keep
+data class Movie(
+    val id: Int,
+    val title: String,
+    val poster: String?,
+    val backdrop: String?,
+    val genres: List<Genre>,
+    val language: String,
+    val longDescription: String,
+    val shortDescription: String?,
+    val status: String?,
+    val rating: Double,
+    val runtime: Int = 0,
+    val releaseDate: String,
+)
