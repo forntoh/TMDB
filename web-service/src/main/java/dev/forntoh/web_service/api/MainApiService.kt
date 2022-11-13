fdb7333432bbec60f68d5bc78d61e7100d63dd67
Forntoh Thomas <thomasforntoh@gmail.com>
2022-11-13 22:19:11 +0100

    added movie discovery

--- web-service/src/main/java/dev/forntoh/web_service/api/MainApiService.kt
+++ web-service/src/main/java/dev/forntoh/web_service/api/MainApiService.kt
@@ -16,10 +16,36 @@
 
 package dev.forntoh.web_service.api
 
+import dev.forntoh.web_service.dto.MoviesDTO
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 /**
  * IMDB API Service Interface
  * all the REST requests for the IMDB API are defined here
  */
 interface MainApiService {
-    
+    /**
+     * Get all the vehicles available within a given bounding box
+     * defined by the top left and bottom right coordinates
+     *
+     * @param page current page
+     * @param sortBy default popularity
+     * @param includeAdult should adult content be included default false
+     * @param includeVideo should video be included
+     * @param releasedBeforeDate movie released before
+     * @param year Release year
+     *
+     * @return list of all the movies
+     */
+    @GET("/discover/movie")
+    suspend fun loadMovies(
+        @Query("page") page: Int,
+        @Query("sort_by") sortBy: String = "popularity.desc",
+        @Query("include_adult") includeAdult: Boolean = false,
+        @Query("include_video") includeVideo: Boolean = false,
+        @Query("release_date.lte") releasedBeforeDate: String? = null,
+        @Query("year.lte") year: String? = null,
+    ): Response<MoviesDTO>
 }
