edaa5b1ec3d5aff075a3c57ab28c89e520f066a6
forntoh <thomasforntoh@gmail.com>
2022-11-14 11:51:12 +0100

    added list state to MoviesList

--- app/src/main/java/dev/forntoh/tmdb/ui/screens/discover/MovieItem.kt
+++ app/src/main/java/dev/forntoh/tmdb/ui/screens/discover/MovieItem.kt
@@ -6,28 +6,36 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.forntoh.common.entities.Movie
+import dev.forntoh.tmdb.utils.OnBottomReached
 
 @Composable
 fun MovieList(
     moviesCollection: List<Movie>,
     onMovieSelect: (Int) -> Unit,
+    loadMore: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val listState = rememberLazyListState()
+
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(16.dp),
         contentPadding = PaddingValues(16.dp),
+        state = listState,
         modifier = modifier
     ) {
         items(moviesCollection) { movie ->
             MovieItem(movie = movie, onMovieSelected = onMovieSelect)
         }
     }
+
+    listState.OnBottomReached(loadMore)
 }
 
 @Composable
