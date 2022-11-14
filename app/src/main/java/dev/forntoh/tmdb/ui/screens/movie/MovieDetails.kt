a4f5ff9be02f3e2d805f1d63be6b670f090f0ba8
forntoh <thomasforntoh@gmail.com>
2022-11-14 10:13:41 +0100

    created empty MovieDetails composable

---
+++ app/src/main/java/dev/forntoh/tmdb/ui/screens/movie/MovieDetails.kt
@@ -0,0 +1,13 @@
+package dev.forntoh.tmdb.ui.screens.movie
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MovieDetails(
+    movieId: Int,
+    movieDetailsViewModel: MovieDetailsViewModel,
+    modifier: Modifier = Modifier
+) {
+
+}
---
+++ app/src/main/java/dev/forntoh/tmdb/ui/screens/movie/MovieDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package dev.forntoh.tmdb.ui.screens.movie
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class MovieDetailsViewModel @Inject constructor() : ViewModel() {
+}
