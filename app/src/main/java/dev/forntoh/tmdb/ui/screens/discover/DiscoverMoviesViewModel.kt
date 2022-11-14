c127a4819cb66bbef1b4662ba22d1b0391d415a9
forntoh <thomasforntoh@gmail.com>
2022-11-14 10:28:22 +0100

    added hilt annotation to view models

--- app/src/main/java/dev/forntoh/tmdb/ui/screens/discover/DiscoverMoviesViewModel.kt
+++ app/src/main/java/dev/forntoh/tmdb/ui/screens/discover/DiscoverMoviesViewModel.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.forntoh.common.entities.DiscoverMoviesFilter
 import dev.forntoh.repository.MoviesRepo
 import dev.forntoh.tmdb.utils.getToDateString
@@ -13,6 +14,7 @@ import kotlinx.coroutines.launch
 import java.util.*
 import javax.inject.Inject
 
+@HiltViewModel
 class DiscoverMoviesViewModel @Inject constructor(
     private val moviesRepo: MoviesRepo
 ) : ViewModel() {
--- app/src/main/java/dev/forntoh/tmdb/ui/screens/movie/MovieDetailsViewModel.kt
+++ app/src/main/java/dev/forntoh/tmdb/ui/screens/movie/MovieDetailsViewModel.kt
@@ -1,7 +1,9 @@
 package dev.forntoh.tmdb.ui.screens.movie
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class MovieDetailsViewModel @Inject constructor() : ViewModel() {
 }
