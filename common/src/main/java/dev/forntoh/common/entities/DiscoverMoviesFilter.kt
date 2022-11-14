efe27338f4c5a6ea16e1082ed6bc4351751f46e9
forntoh <thomasforntoh@gmail.com>
2022-11-14 09:39:06 +0100

    page now var

--- common/src/main/java/dev/forntoh/common/entities/DiscoverMoviesFilter.kt
+++ common/src/main/java/dev/forntoh/common/entities/DiscoverMoviesFilter.kt
@@ -1,7 +1,7 @@
 package dev.forntoh.common.entities
 
 data class DiscoverMoviesFilter(
-    val page: Int = 1,
+    var page: Int = 1,
     val sortBy: String = "popularity.desc",
     val includeAdult: Boolean = false,
     val includeVideo: Boolean = false,
