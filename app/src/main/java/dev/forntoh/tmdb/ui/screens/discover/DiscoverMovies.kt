3763227f05f31ac0751064b2fad05e1ceba383b0
forntoh <thomasforntoh@gmail.com>
2022-11-14 10:12:53 +0100

    created empty DiscoverMovies composable

---
+++ app/src/main/java/dev/forntoh/tmdb/ui/screens/discover/DiscoverMovies.kt
@@ -0,0 +1,13 @@
+package dev.forntoh.tmdb.ui.screens.discover
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DiscoverMovies(
+    discoverMoviesViewModel: DiscoverMoviesViewModel,
+    onMovieSelected: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+}
