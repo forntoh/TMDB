b81c2f6bc7b04d9bffd72fd1afba0714880637a1
forntoh <thomasforntoh@gmail.com>
2022-11-14 11:41:21 +0100

    discover movies screen

--- app/src/main/java/dev/forntoh/tmdb/ui/screens/discover/DiscoverMovies.kt
+++ app/src/main/java/dev/forntoh/tmdb/ui/screens/discover/DiscoverMovies.kt
@@ -1,7 +1,10 @@
 package dev.forntoh.tmdb.ui.screens.discover
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import dev.forntoh.common.entities.Movie
 
 @Composable
 fun DiscoverMovies(
@@ -10,4 +13,24 @@ fun DiscoverMovies(
     modifier: Modifier = Modifier
 ) {
 
+    val moviesCollection by discoverMoviesViewModel.movies.observeAsState(emptyList())
+
+    DiscoverMovies(
+        moviesCollection = moviesCollection,
+        onMovieSelected = onMovieSelected,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun DiscoverMovies(
+    moviesCollection: List<Movie>,
+    onMovieSelected: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    MovieList(
+        moviesCollection = moviesCollection,
+        onMovieSelect = onMovieSelected,
+        modifier = modifier
+    )
 }
