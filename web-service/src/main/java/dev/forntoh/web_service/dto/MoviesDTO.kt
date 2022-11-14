74356e45286c854198f5cb572916fe3acddd6793
forntoh <thomasforntoh@gmail.com>
2022-11-14 12:37:47 +0100

    fixed serialization issue

--- web-service/src/main/java/dev/forntoh/web_service/dto/MoviesDTO.kt
+++ web-service/src/main/java/dev/forntoh/web_service/dto/MoviesDTO.kt
@@ -1,7 +1,9 @@
 package dev.forntoh.web_service.dto
 
+import com.google.gson.annotations.SerializedName
+
 data class MoviesDTO(
     val page: Int,
-    val results: List<MovieDTO> = emptyList(),
-    val totalPages: Int
+    var results: List<MovieDTO> = emptyList(),
+    @SerializedName("total_pages") val totalPages: Int
 )
