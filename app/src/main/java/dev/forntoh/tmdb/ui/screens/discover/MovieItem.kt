5335b08879773c5c06d0924d2bed6e12a5ea782f
forntoh <thomasforntoh@gmail.com>
2022-11-15 10:28:15 +0100

    added simple MovieItem design for search

--- app/src/main/java/dev/forntoh/tmdb/ui/screens/discover/MovieItem.kt
+++ app/src/main/java/dev/forntoh/tmdb/ui/screens/discover/MovieItem.kt
@@ -1,5 +1,6 @@
 package dev.forntoh.tmdb.ui.screens.discover
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -8,11 +9,16 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import dev.forntoh.common.entities.Movie
 import dev.forntoh.tmdb.utils.OnBottomReached
@@ -28,7 +34,7 @@ fun MovieList(
 
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(start = 16.dp, end = 16.dp, bottom = 16.dp, top = 86.dp),
+        contentPadding = PaddingValues(16.dp),
         state = listState,
         modifier = modifier
     ) {
@@ -85,6 +91,47 @@ fun MovieItem(
                     color = MaterialTheme.colors.secondary
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun MovieItem(
+    movie: Movie
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(TextFieldDefaults.MinHeight),
+        horizontalArrangement = Arrangement.Start
+    ) {
+        AsyncImage(
+            model = movie.poster,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .background(Color.DarkGray)
+                .aspectRatio(1f, true)
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(2.dp)
+        ) {
+            Text(
+                text = movie.title,
+                style = MaterialTheme.typography.subtitle2,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = movie.longDescription,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                fontSize = 11.sp,
+                modifier = Modifier.alpha(0.5f)
+            )
         }
     }
 }
